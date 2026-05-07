9b2a475b708d36be873d888629b3e48d95668dd0
Eyrol Clayford M. Junio <engkoyjunio@gmail.com>
2026-05-07 10:48:23 +0800

    new and updated

--- src/main.js
+++ src/main.js
@@ -2,19 +2,16 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
+import './assets/styles.scss';  // ← change this line
 
 import Aura from '@primeuix/themes/aura';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
 
-import '@/assets/styles.scss';
-
 const app = createApp(App);
-const pinia = createPinia()
-app.use(pinia)
-app.use(router)
-
+const pinia = createPinia();
+app.use(pinia);
 app.use(router);
 app.use(PrimeVue, {
     theme: {
